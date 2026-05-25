e02712b68b6979ae1aa53db4f9c44d02ea6b57f8
MdAfsarHossain <afsar291722@gmail.com>
2026-05-25 21:22:17 +0600

    feat: implement CRUD API endpoints for demotable operations

--- index.js
+++ index.js
@@ -29,6 +29,95 @@ client.connect().then(() => {
 //     client.end();
 // });
 
+app.post('/postData', (req, res) => {
+    const { name, id } = req.body;
+    const insert_query = `INSERT INTO demotable (name, id) VALUES ($1, $2)`
+
+    client.query(insert_query, [name, id], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error inserting data');
+        }
+        else {
+            // console.log(result);
+            // res.status(200).send(result.rows)
+            res.status(200).json({ message: 'Data inserted successfully' });
+        }
+    })
+
+})
+
+app.get('/getData', (req, res) => {
+    const select_query = `SELECT * FROM demotable`
+
+    client.query(select_query, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error fetching data');
+        }
+        else {
+            // console.log(result);
+            // res.status(200).send(result.rows)
+            res.status(200).json({ message: 'Data fetched successfully', data: result.rows });
+        }
+    })
+
+})
+
+app.get('/getSpecificData/:id', (req, res) => {
+    const { id } = req.params;
+    const select_query = `SELECT * FROM demotable WHERE id = $1`
+
+    client.query(select_query, [id], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error fetching data');
+        }
+        else {
+            // console.log(result);
+            // res.status(200).send(result.rows)
+            res.status(200).json({ message: 'Data fetched successfully', data: result.rows });
+        }
+    })
+
+})
+
+app.patch('/updateData/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    const update_query = `UPDATE demotable SET name = $1 WHERE id = $2`
+
+    client.query(update_query, [name, id], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error updating data');
+        }
+        else {
+            // console.log(result);
+            // res.status(200).send(result.rows)
+            res.status(200).json({ message: 'Data updated successfully' });
+        }
+    })
+
+})
+
+app.delete('/deleteData/:id', (req, res) => {
+    const { id } = req.params;
+    const delete_query = `DELETE FROM demotable WHERE id = $1`
+
+    client.query(delete_query, [id], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error deleting data');
+        }
+        else {
+            // console.log(result);
+            // res.status(200).send(result.rows)
+            res.status(200).json({ message: 'Data deleted successfully' });
+        }
+    })
+
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
