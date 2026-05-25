a404667bd29f2611deb68d922092e17be55967fa
MdAfsarHossain <afsar291722@gmail.com>
2026-05-25 21:21:33 +0600

    feat: initialize PostgreSQL client connection and start Express server on port 3000

--- index.js
+++ index.js
@@ -4,3 +4,32 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'afsar',
+    database: 'demopost'
+});
+
+client.connect().then(() => {
+    console.log('Connected to database');
+}).catch((err) => {
+    console.error(err);
+})
+// .finally(() => {
+//     client.end();
+// });
+
+// client.query('SELECT * FROM users', (err, res) => {
+//     if (err) {
+//         console.error(err);
+//     }
+//     console.log(res.rows);
+//     client.end();
+// });
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
